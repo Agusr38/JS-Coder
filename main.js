b5d5ccc1e6d458d3cf754dd989c294a1cf2e6d7d
Agustin Ruiz <agustinruiz38@gmail.com>
2022-09-27 21:48:15 -0300

    primerentrega

---
+++ main.js
@@ -0,0 +1,19 @@
+let precioPorDia = 10000;
+
+
+
+
+
+let nombreInquilino = prompt("nombre completo");
+let mailInquilino = prompt("ingresu su mail");
+let cantPersonas = parseInt(prompt("cuantas personas serian?"));
+let cantDiasAlquier = parseInt(prompt("cuantos dias queres alquilar?"));
+console.log(nombreInquilino);
+
+
+let precioTotal = cantDiasAlquier*precioPorDia
+console.log(precioTotal)
+
+if(cantPersonas) {
+    precioPorDia =
+}
