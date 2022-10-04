dc5edbe010aece71b915d861d94dcbb4730d0db9
Agustin Ruiz <agustinruiz38@gmail.com>
2022-10-04 20:40:17 -0300

    conif

--- main.js
+++ main.js
@@ -1,19 +1,52 @@
-let precioPorDia = 10000;
-
-
-
-
-
 let nombreInquilino = prompt("nombre completo");
-let mailInquilino = prompt("ingresu su mail");
-let cantPersonas = parseInt(prompt("cuantas personas serian?"));
-let cantDiasAlquier = parseInt(prompt("cuantos dias queres alquilar?"));
-console.log(nombreInquilino);
+console.log("Hola " ,nombreInquilino)
 
+let cantDiasAlquier = parseInt(prompt("cuantos dias queres alquilar?"));
+console.log("Dias solicitados: " ,cantDiasAlquier)
+let cantPersonas = parseInt(prompt("cuantas personas serian?"));
+console.log("cantidad de personas : " ,cantPersonas)
+
+
+
+if(cantPersonas<=4) {
+    console.log("Tengo disponible la casa Los Molles")
+    precioPorDia = 10000
+    
+}else if (cantPersonas==5){
+    console.log("Tengo disponibilidad en la casa Los Espinillos(1) o en la casa Los Robles(2)")
+    let casaElegida = prompt( "Que casa deseas? 1 o 2?")
+   
+    
+    
+    console.log(casaElegida)
+        if (casaElegida == 1){
+            precioPorDia = 12000
+        } 
+        else{
+            precioPorDia = 15000
+        }
+
+}else if (cantPersonas==6){
+    console.log("Tengo disponibilidad en la casa Los Espinillos(1) o en la casa Los Robles(2)")
+    let casaElegida = prompt( "Que casa deseas? 1 o 2?")
+    
+  
+    
+    console.log(casaElegida)
+        if (casaElegida == 1){
+            precioPorDia = 13000
+        } 
+        else{
+            precioPorDia = 18000
+        }
+}
+else{
+    alert("No tengo disponibilidad  para esa cantidad de personas")
+}
 
 let precioTotal = cantDiasAlquier*precioPorDia
-console.log(precioTotal)
+console.log("El costo total de tu estadia  es de ",precioTotal)
 
-if(cantPersonas) {
-    precioPorDia =
-}
+
+
+
