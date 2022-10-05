3804d90877a3ff750374a0b1189abec66c512ef1
Agustin Ruiz <agustinruiz38@gmail.com>
2022-10-05 11:21:55 -0300

    funcion

--- main.js
+++ main.js
@@ -44,8 +44,28 @@ else{
     alert("No tengo disponibilidad  para esa cantidad de personas")
 }
 
+
+
 let precioTotal = cantDiasAlquier*precioPorDia
 console.log("El costo total de tu estadia  es de ",precioTotal)
+
+let FormaDePago = prompt( "Como deseas abonar?:Tarjeta Credito (1) o Transferencia(2)")
+
+
+if (FormaDePago == 1){
+console.log (" El precio de tu estadia pagando con tarjeta es de :",precioRecargo())
+}
+else{
+    console.log (" El precio de tu estadia pagando con transferencia  es de :",precioTotal)
+
+}
+function precioRecargo(){
+    let precioRecargoT = ((precioPorDia*cantDiasAlquier)*1.10) ;
+    return precioRecargoT;
+}
+
+
+
 
 
 
